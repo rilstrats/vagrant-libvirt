4885e57947b8ced643e1b9b7f7a379d75f3d7919
Riley <rilstrats@pm.me>
2022-12-02 18:07:26 -0700

    default graphical

--- cit352/fedora.sh
+++ cit352/fedora.sh
@@ -19,6 +19,9 @@ systemctl enable --now gdm
 # dnf -yq group install "KDE Plasma Workspaces"
 # systemctl enable --now sddm
 
+# GUI
+systemctl set-default graphical
+
 # guest agents
 echo "Setting Up Guest Agents"
 dnf install -yq qemu-guest-agent spice-vdagent
@@ -26,7 +29,7 @@ systemctl start qemu-guest-agent # shutdown, reboot
 systemctl start spice-vdagentd # clipboard, better GUI
 
 # xresize (only needed if using GUI and it doesn't automatically resize)
-echo "Adding xresize Scripts"
-ln -s /vagrant/xresize/xresize /vagrant/xresize/xresize-loop /usr/local/bin
+# echo "Adding xresize Scripts"
+# ln -s /vagrant/xresize/xresize /vagrant/xresize/xresize-loop /usr/local/bin
 # ln -s /vagrant/xresize/xresize-loop.desktop /home/vagrant/.config/autostart
 
